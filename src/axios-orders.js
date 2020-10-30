c05586a1dac15f11fb1f738cc61555d922a21e56
PJ Lindsay <pjlindsay@gmail.com>
2020-10-30 17:10:37 -0600

    POST order to firebase

---
+++ src/axios-orders.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+import firebaseurl from './firebase.js'
+
+const instance = axios.create({
+  // baseURL: 'https://burger-builder-be3c9.firebaseio.com/'
+  baseURL: firebaseurl
+})
+
+export default instance;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,8 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
+import axios from '../../axios-orders';
+
 const INGREDIENT_PRICES = {
   lettuce: 0.5,
   cheese: 0.4,
@@ -70,8 +72,26 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    // this.setState({ purchasing: true });
-    alert('Your order will now continue')
+    // alert('Your order will now continue')
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: 'PJ Burgerssen',
+        address: {
+          street: '123 Main Street',
+          zipCode: '332257',
+          country: 'Canada'
+        },
+        email: 'test@localhost.com'
+      },
+      deliveryMethod: 'fastest'
+    }
+
+    axios.post('/orders.json', order)
+      .then(response => console.log(response))
+      .catch(error=> console.error(error))
+
   }
 
   render () {
