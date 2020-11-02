be660d1e564e5c8b91876b7d43b99bdf65bfed74
PJ Lindsay <pjlindsay@gmail.com>
2020-11-02 09:22:29 -0700

    Add Orders page and route

---
+++ src/containers/Orders/Orders.js
@@ -0,0 +1,17 @@
+const React, { Component } = require("react");
+
+import Order from '../../components/Order/Order';
+
+class Orders extends Component {
+
+  render() {
+    return (
+      <div>
+        <Order />
+        <Order />
+      </div>
+    )
+  }
+}
+
+export default Orders;
