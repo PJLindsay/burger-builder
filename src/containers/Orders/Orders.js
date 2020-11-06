bfeae61900065c0b5544a3d16c3bb3a88f661925
PJ Lindsay <pjlindsay@gmail.com>
2020-11-06 11:35:16 -0700

    Fix missing userId property on fetchOrders

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -10,6 +10,7 @@ import Spinner from '../../components/UI/Spinner/Spinner';
 class Orders extends Component {
 
   componentDidMount() {
+    console.log('Fetching orders? --> ', this.props)
     this.props.onFetchOrders(this.props.token, this.props.userId);
   }
 
@@ -39,6 +40,7 @@ const mapStateToProps = state => {
     orders: state.order.orders,
     loading: state.order.loading,
     token: state.auth.token,
+    userId: state.auth.userId
   }
 }
 
