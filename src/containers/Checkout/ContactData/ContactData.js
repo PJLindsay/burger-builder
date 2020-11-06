3679cc65310abd66229aaed1652c8b5d46ea42e2
PJ Lindsay <pjlindsay@gmail.com>
2020-11-06 10:01:57 -0700

    Refactor to share checkValidity

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -9,7 +9,7 @@ import styles from './ContactData.module.css';
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
 
 import * as actions from '../../../store/actions/index'
-import { updateObject } from '../../../shared/utility';
+import { checkValidity, updateObject } from '../../../shared/utility';
 
 class ContactData extends Component {
   state = {
@@ -115,35 +115,12 @@ class ContactData extends Component {
 
   }
 
-  checkValidity(value, rules) {
-    let isValid = true;
-
-    if (!rules) {
-      return true;
-    }
-
-    if (rules.required) {
-      isValid = value.trim() !== '' && isValid;
-    }
-
-    if (rules.minLength) {
-      isValid = value.length >= rules.minLength && isValid;
-    }
-
-    if (rules.maxLength) {
-      isValid = value.length <= rules.maxLength && isValid;
-    }
-
-    return isValid
-  }
-
   // this is an example of immutable object style (core react concept)
   inputChangedHandler = (event, inputIdentifier) => {
 
-    // NOTE2: so we need to get form element values too
     const updatedFormElement = updateObject(this.state.orderForm[inputIdentifier], {
       value: event.target.value,
-      valid: this.checkValidity(event.target.value, this.state.orderForm[inputIdentifier].validation),
+      valid: checkValidity(event.target.value, this.state.orderForm[inputIdentifier].validation),
       touched: true
     });
 
