92283a05bbbd23b425f723828bc6f542ae357c0b
PJ Lindsay <pjlindsay@gmail.com>
2020-11-05 20:59:20 -0700

    Add Auto-logout (token expiration)

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,11 +1,11 @@
-import axios from 'axios';
-import * as actionTypes from './actionTypes';
+import axios from 'axios'
+import * as actionTypes from './actionTypes'
 
-import {FB_API_KEY} from '../../firebase';
+import { FB_API_KEY } from '../../firebase'
 
 export const authStart = () => {
   return {
-    type: actionTypes.AUTH_START
+    type: actionTypes.AUTH_START,
   }
 }
 
@@ -13,38 +13,53 @@ export const authSuccess = (token, userId) => {
   return {
     type: actionTypes.AUTH_SUCCESS,
     idToken: token,
-    userId: userId
+    userId: userId,
   }
 }
 
 export const authFail = (error) => {
   return {
     type: actionTypes.AUTH_FAIL,
-    error: error
+    error: error,
+  }
+}
+
+export const logout = () => {
+  return {
+    type: actionTypes.AUTH_LOGOUT
+  }
+}
+
+export const checkAuthTimeout = (expirationTime) => {
+  return (dispatch) => {
+    setTimeout(() => {
+      dispatch(logout());
+    }, expirationTime * 1000);
   }
 }
 
 export const auth = (email, password, isSignup) => {
-  return dispatch => {
-    dispatch(authStart());
+  return (dispatch) => {
+    dispatch(authStart())
     const authData = {
       email: email,
       password: password,
-      returnSecureToken: true
+      returnSecureToken: true,
     }
 
-    let url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${FB_API_KEY}`;
+    let url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${FB_API_KEY}`
 
     if (!isSignup) {
-      url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${FB_API_KEY}`;
+      url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${FB_API_KEY}`
     }
 
-    axios.post(url, authData)
-      .then(response => {
-        console.log(response);
+    axios
+      .post(url, authData)
+      .then((response) => {
         dispatch(authSuccess(response.data.idToken, response.data.localId))
+        dispatch(checkAuthTimeout(response.data.expiresIn))
       })
-      .catch(err => {
+      .catch((err) => {
         dispatch(authFail(err.response.data.error))
       })
   }
